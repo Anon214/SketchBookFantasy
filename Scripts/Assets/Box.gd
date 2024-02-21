7af4567a98971c189c841710ac2f4f855c91c063
Anon214 <anonrahman120@gmail.com>
2024-02-21 17:44:22 -0500

    finished box puzzle

--- Scripts/Assets/Box.gd
+++ Scripts/Assets/Box.gd
@@ -2,38 +2,31 @@ class_name Box
 extends Area2D
 
 var area_index : int = 0
-var r : int = 0
-var c : int = 0
+var r : int = -1
+var c : int = -1
 @onready var grid_container = %GridContainer
 
 func _on_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	if area.name == "Left" or area.name == "Right" or area.name == "Top" or area.name == "Bot":
+		return
+		
 	area_index = area_shape_index
-	calculate_matrix()
-	grid_container.set_boxes(self)
+	if r == -1:
+		calculate_matrix()
+		grid_container.set_box(self)
 	
 func calculate_matrix():
 	r = int(area_index / 6)
 	c = area_index % 6
 
-func check_valid():
-	pass
-
-
 func _on_left_body_entered(body):
-	if body is Player:
-		pass
-
-
-func _on_top_body_entered(body):
-	if body is Player:
-		pass
-
+	grid_container.set_box_left(r, c)
 
 func _on_right_body_entered(body):
-	if body is Player:
-		pass
+	grid_container.set_box_right(r, c)
 
+func _on_top_body_entered(body):
+	grid_container.set_box_top(r, c)
 
-func _on_down_body_entered(body):
-	if body is Player:
-		pass
+func _on_bot_body_entered(body):
+	grid_container.set_box_down(r, c)
--- Scripts/Assets/GridContainer.gd
+++ Scripts/Assets/GridContainer.gd
@@ -4,6 +4,8 @@ extends Area2D
 @onready var children = get_children()
 @onready var collision_matrix = create_empty_collision_matrix()
 @onready var box_matrix = create_empty_box_matrix()
+var total : int = 0
+var rowToCol = {0:[], 1:[2, 3], 2:[1, 4], 3:[1, 2, 3, 4], 4:[1, 4], 5:[]}
 
 func create_empty_collision_matrix():
 	var matrix = []
@@ -16,7 +18,7 @@ func create_empty_collision_matrix():
 		matrix.append(array)
 	
 	return matrix
-		
+	
 func create_empty_box_matrix():
 	var matrix = []
 	for i in range(6):
@@ -24,14 +26,75 @@ func create_empty_box_matrix():
 		matrix.append(array)
 	return matrix
 		
-func set_boxes(box: Box):
+		
+func invalid(r, c):
+	if r < 0 or c < 0 or r > 5 or c > 5 or box_matrix[r][c]:
+		return true
+	return false
+	
+func set_box(box: Box):
 	var r = box.r
 	var c = box.c
 	box_matrix[r][c] = box
-	print(r, c)
 	
 	var pos = collision_matrix[r][c].position
-	pos.x -= 56
+	pos.x -= 57
 	pos.y -= 16
 	
 	box.position = pos
+	
+	if c in rowToCol[r]:
+		total += 1
+	
+	if total == 10:
+		print("win")
+	
+	
+		
+func moved_outside_ans(r, c):
+	if c in rowToCol[r]:
+		total -= 1
+
+func set_box_left(r, c): #0,1
+	if invalid(r, c + 1):
+		return
+		
+	moved_outside_ans(r, c)
+	
+	var box = box_matrix[r][c]
+	box_matrix[r][c] = 0
+	box.c = box.c + 1
+	set_box(box)
+	
+func set_box_right(r, c):
+	if invalid(r, c - 1):
+		return
+	
+	moved_outside_ans(r, c)
+	
+	var box = box_matrix[r][c]
+	box_matrix[r][c] = 0
+	box.c = box.c - 1
+	set_box(box)
+	
+func set_box_top(r, c):
+	if invalid(r + 1, c):
+		return
+		
+	moved_outside_ans(r, c)
+	
+	var box = box_matrix[r][c]
+	box_matrix[r][c] = 0
+	box.r = box.r + 1
+	set_box(box)
+	
+func set_box_down(r, c):
+	if invalid(r - 1, c):
+		return
+		
+	moved_outside_ans(r, c)
+	
+	var box = box_matrix[r][c]
+	box_matrix[r][c] = 0
+	box.r = box.r - 1
+	set_box(box)
